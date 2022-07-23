da05690f4ebd28c30cce494f4d703214e57bcebc
aaronbading <aaronbading.ab@gmail.com>
2022-07-22 20:18:15 -0500

    setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pybind11-examples
 This is an assortment of pybind11 C++ / Python code examples.
+
+
+## Building
+
+```bash
+$mkdir build
+$cd build
+$cmake ..
+$make
+```
---
+++ pybind_example/build/CMakeFiles/PriceAlgo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PriceAlgo.dir/src/main.cpp.o"
+  "PriceAlgo.cpython-39-x86_64-linux-gnu.so"
+  "PriceAlgo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PriceAlgo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
